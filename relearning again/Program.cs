253cd872937bb4280fef492607c3f8a73c8856b2
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2024-01-17 10:32:53 +0100

    Making more projects with c#

--- relearning again/Program.cs
+++ relearning again/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace relearning_again
+{
+    class Program
+    {
+      static void Main()
+        {
+            List<string> options = new List<string>();
+            options.Add("Cat");
+            options.Add("Lion");
+            options.Add("Tiger");
+
+            Console.WriteLine("what is the name of the largest cat?");
+            int number = 1;
+            for(int i = 0; i < options.Count; i++)
+            {
+                options[i] = $"{number}. {options[i]}";
+
+                number++;
+            }
+
+            foreach(string option in options) {
+                Console.WriteLine(option);
+            }
+            Console.ReadLine();
+          
+
+
+
+            Console.ReadKey();
+        }
+
+        
+    }
+
+}
