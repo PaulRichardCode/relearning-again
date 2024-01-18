4fac05ce43e692884103edc8c85130468f1e1c94
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2024-01-18 19:54:31 +0100

    jhjkjk

--- relearning again/Program.cs
+++ relearning again/Program.cs
@@ -13,20 +13,7 @@ namespace relearning_again
     {
       static void Main()
         {
-            int num1;
-            int num2;
-            string answer;
-
-            Console.WriteLine("welcome to the calculator programme");
-            Console.WriteLine("Please enter your first number");
-
-           num1 =  Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter your second number");
-            num2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(".......");
-            Thread.Sleep(3000);
-
+            
 
 
             Console.ReadKey();
